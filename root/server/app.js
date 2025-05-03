de5e89f8a99c9b554b580c7a1649969623fbeae9
Yaj Patel <yajpatel3594@gmail.com>
2025-05-03 07:34:04 +0530

    url try

--- root/server/app.js
+++ root/server/app.js
@@ -104,7 +104,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors(
     {
-        origin : "https://quick-share-olda.onrender.com",
+        origin : "https://quickshare-096t.onrender.com/upload",
+        // origin : "https://quick-share-olda.onrender.com",
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type', 'Authorization']
     }
